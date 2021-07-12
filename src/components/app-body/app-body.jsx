9d0a846941f7db734025834fbc4eac01ede8e995
Nithyanandh David Xavier <xdavid321@gmail.com>
2021-07-12 16:46:55 +0530

    added styles for table

--- src/components/app-body/app-body.jsx
+++ src/components/app-body/app-body.jsx
@@ -2,12 +2,14 @@ import { Component } from 'react';
 import { TextField,Box,makeStyles } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
+// import StyledTableCell from '@material-ui/core/StyledTableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import styles from './app-body.module.sass';
+
+import StyledTableCell from "../styled-components/table-cell";
 
 const useStyles = makeStyles({
     table: {
@@ -19,21 +21,23 @@ class AppBody extends Component {
 
     render() {
         return(
-            <Box>
-                <Table size="small">           
-            <TableRow>
-            <TableCell>Caller ID</TableCell>
-            <TableCell align="right"><TextField className={styles.TextField} id="caller-id" label="Caller ID" variant="outlined" size="small" margin="dense" /></TableCell>
-            <TableCell align="right">DNIS</TableCell>
-            <TableCell align="right"><TextField className={styles.TextField} id="dnis" label="DNIS" variant="outlined" size="small" margin="dense" /></TableCell>            
-            </TableRow>
-            <TableRow>
-            <TableCell>ANI</TableCell>
-            <TableCell align="right"><TextField className={styles.TextField}  id="ani" label="ANI" variant="outlined" size="small" /></TableCell>
-            <TableCell align="right">Callback Memo</TableCell>
-            <TableCell align="right"><TextField className={styles.TextField} id="callback memo" label="Callback Memo" variant="outlined" size="small" /></TableCell>
-          </TableRow>
-          </Table> 
+            <Box className={styles.box}>
+              <Table size="small" className={styles.table}>           
+                <TableBody>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>Caller ID</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="caller-id" label="Caller ID" variant="outlined" size="small" margin="dense" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">DNIS</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="dnis" label="DNIS" variant="outlined" size="small" margin="dense" /></StyledTableCell>            
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>ANI</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField}  id="ani" label="ANI" variant="outlined" size="small" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">Callback Memo</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="callback memo" label="Callback Memo" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
             </Box>
         )
     }
--- src/components/styled-components/table-cell.jsx
+++ src/components/styled-components/table-cell.jsx
@@ -3,11 +3,11 @@ import { TableCell, withStyles } from '@material-ui/core';
 export default withStyles(() => ({
     head: {
         fontWeight: 'bold',
-        border: '0.5px solid #000',
+        border: '0.5px solid #ddd',
         fontSize: '12px'
     },
     body: {
-        border: '0.5px solid #000',
+        border: '0.5px solid #ddd',
         fontSize: '12px'
     }
 }))(TableCell);

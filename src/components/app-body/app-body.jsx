cfdf6f27d67847446ba600eca7e78cdfdbaf61a8
Sakthi <ssakthi8@deloitte.com>
2021-07-12 17:52:23 +0530

    app-body added more rows

--- src/components/app-body/app-body.jsx
+++ src/components/app-body/app-body.jsx
@@ -40,6 +40,46 @@ class AppBody extends Component {
                     <StyledTableCell className={styles.tableCell} align="right">Callback Memo</StyledTableCell>
                     <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="callback memo" variant="outlined" size="small" /></StyledTableCell>
                   </TableRow>
+                  <TableRow>
+                  <StyledTableCell className={styles.tableCell}>Service Name</StyledTableCell>
+                  <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="service name" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>HIPAA Verification</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField}  id="hipaaverification" variant="outlined" size="small" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">Beneficiary ID</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="beneficiaryid" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>First Name</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField}  id="firstname" variant="outlined" size="small" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">Last Name</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="lastname" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                  <StyledTableCell className={styles.tableCell}>DOB</StyledTableCell>
+                  <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="dob" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                  <StyledTableCell className={styles.tableCell}>Short Message</StyledTableCell>
+                  <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="shortmessage" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>Provider ID</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField}  id="providerid" variant="outlined" size="small" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">IVR Transaction Number</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="ivrtransactionnumber" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell className={styles.tableCell}>Caller Type</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField}  id="callertype" variant="outlined" size="small" /></StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right">Caller Intent</StyledTableCell>
+                    <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="callerintent" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                  <StyledTableCell className={styles.tableCell}>Bene Plan Code</StyledTableCell>
+                  <StyledTableCell className={styles.tableCell} align="right"><TextField className={styles.TextField} id="beneplancode" variant="outlined" size="small" /></StyledTableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </Box>
